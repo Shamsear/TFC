725de2db50f21d91ffcd165f514bb9aaf9058391
Shamsear <4pa20cs104@pace.edu.in>
2026-06-03 11:05:41 +0530

    Update route.ts

--- app/api/tournaments/[tournamentId]/team-awards/route.ts
+++ app/api/tournaments/[tournamentId]/team-awards/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { auth } from '@/lib/auth'
-import prisma from '@/lib/prisma'
+import { prisma } from '@/lib/prisma'
 import { generateId } from '@/lib/id-generator'
 
 // GET: Fetch all team awards for a tournament
